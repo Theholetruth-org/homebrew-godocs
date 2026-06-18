7022426c7ec01ffaa107b15887e10f7364d0aed4
theholepaperclip <289486043+theholepaperclip@users.noreply.github.com>
2026-06-18 05:48:05 +0000

    godocs-nightly 2026.06.18

---
+++ Formula/godocs-nightly.rb
@@ -0,0 +1,27 @@
+class GodocsNightly < Formula
+  desc "Evidence-ETL CLI (nightly build from main) -- split, Bates-stamp, and RAG-chunk PDF disclosure bundles"
+  homepage "https://github.com/Theholetruth-org/Hole-GoDocs"
+  version "2026.06.18"
+  license :cannot_represent
+
+  conflicts_with "godocs", because: "both install the same godocs binary"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/Theholetruth-org/Hole-GoDocs/releases/download/nightly/godocs-nightly-macos-arm64.tar.gz"
+      sha256 "31f5920bd9a02fb0e605cc5dd8a8a617357de440b5dcdf9c68e48ff01f6acc10"
+    else
+      url "https://github.com/Theholetruth-org/Hole-GoDocs/releases/download/nightly/godocs-nightly-macos-amd64.tar.gz"
+      sha256 "ba2cb4872f3b538b61ae6a7a18338f8327aef3be8dc61eea5c6841e9f2061804"
+    end
+  end
+
+  def install
+    bin.install "godocs"
+    bin.install "godocs-pdf-merge"
+  end
+
+  test do
+    assert_match "nightly", shell_output("#{bin}/godocs --version")
+  end
+end

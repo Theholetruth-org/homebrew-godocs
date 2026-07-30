d32d5eb5403994a69f78ec3646ffc719a1398972
theholepaperclip <289486043+theholepaperclip@users.noreply.github.com>
2026-07-30 05:10:06 +0000

    holedocs-nightly 2026.07.30

---
+++ Formula/holedocs-nightly.rb
@@ -0,0 +1,43 @@
+class HoledocsNightly < Formula
+  desc "Evidence-ETL CLI (nightly build from main) -- split, Bates-stamp, and RAG-chunk PDF disclosure bundles"
+  homepage "https://github.com/Theholetruth-org/Hole-GoDocs"
+  version "2026.07.30"
+  license :cannot_represent
+
+  depends_on "mupdf-tools"
+  depends_on "qpdf"
+
+  conflicts_with "holedocs", because: "both install the same godocs binary"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/Theholetruth-org/Hole-GoDocs/releases/download/nightly/godocs-nightly-macos-arm64.tar.gz"
+      sha256 "5fa9a68331ab050a79e7d4e94b6445a9981a013dcb50c12b7ae3328d851fcba2"
+    else
+      url "https://github.com/Theholetruth-org/Hole-GoDocs/releases/download/nightly/godocs-nightly-macos-amd64.tar.gz"
+      sha256 "4db20829e4013b79edc5462bf8d8add1e518ac481bc5e0d9dc00489b779c9705"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/Theholetruth-org/Hole-GoDocs/releases/download/nightly/godocs-nightly-linux-arm64.tar.gz"
+      sha256 "7020bb22d1ffe8d6953d19fd9bedd0bba5e38d96c72267d29c4c4ba49d37d315"
+    else
+      url "https://github.com/Theholetruth-org/Hole-GoDocs/releases/download/nightly/godocs-nightly-linux-amd64.tar.gz"
+      sha256 "7a38d375d473056109f5393930c7e149d0da726362029ea0007db2184774a2f6"
+    end
+  end
+
+  def install
+    bin.install "godocs"
+    bin.install "godocs-pdf-merge"
+    bash_completion.install "completions/godocs.bash" => "godocs"
+    zsh_completion.install "completions/_godocs"
+    man1.install "man/godocs.1"
+  end
+
+  test do
+    assert_match "nightly", shell_output("#{bin}/godocs --version")
+  end
+end
